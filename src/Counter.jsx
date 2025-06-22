148475ac8e726a7c260ca74361b8a4468b07d660
Gealah <weerasinruan@gmail.com>
2025-06-22 03:06:57 +0000

    simple calculator

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,14 +1,39 @@
 import { useState } from "react";
 export default function Counter(){
-    const [count,setCount]=useState(0);
-    return (<div>
-    <button onClick={()=>setCount(count+1)}>
-        Click me
-    </button> <button onClick={()=>setCount(0)}>
-        Reset
-    </button>
-    <p>You clicked {count} times
-    </p>
+    const [count, setCount] = useState(0);
+    const [celciusKm, setCelciusKm] = useState("");
+    const [megabyte, setMegabyte] = useState("");
+    const [centimeter, setCentimeter] = useState("");
 
-    </div>);
+    return (
+      <div style={{ textAlign: "center" }}>
+        <input
+          value={celciusKm}
+          onChange={e => setCelciusKm(e.target.value)}
+        /> Celcius/Km
+        <p>{celciusKm} Celcius to {celciusKm * 9 / 5 + 32} Farenheit</p>
+        <p>{celciusKm} Kilometer to {celciusKm / 1.6} Farenheit</p>
+
+        <button onClick={() => setCount(count - 1)}>-1</button>
+        <button onClick={() => setCount(0)}>Reset</button>
+        <button onClick={() => setCount(count + 1)}>+1</button>
+        <p>You clicked {count} times</p>
+
+        <input
+          value={megabyte}
+          onChange={e => setMegabyte(e.target.value)}
+        /> Megabyte
+        <p>
+          {megabyte} Megabyte(MB) is equivalent to <h2>{megabyte / 1000}</h2> Gigabyte(GB)
+        </p>
+
+        <input
+          value={centimeter}
+          onChange={e => setCentimeter(e.target.value)}
+        /> Centimeter
+        <p>
+          ({centimeter}) Centimeter is equivalent to <h2>{centimeter / 2.54}</h2> Inches
+        </p>
+      </div>
+    );
 }
--- src/index.jsx
+++ src/index.jsx
@@ -4,7 +4,7 @@ import './index.css';
 //import App from './App';
 //import Hello from './Hello' ;
 import Counter from './Counter';
-import reportWebVitals from './reportWebVitals';
+import reportWebVitals from './reportWebVitals'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
